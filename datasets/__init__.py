c27346c164b539ca48ababaedfd001a72db46a64
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2024-02-11 21:09:03 +0530

    DCASE19 Added

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,17 +1,20 @@
 # Dataset utils
 from .my_esc10 import ESC10
 from .my_esc50 import ESC50
+from .my_dcase19 import DCASE19
 from .my_fsc import FSC
 
 NAMES = {
     ESC10.NAME: ESC10,
     ESC50.NAME: ESC50,
+    DCASE19.NAME: DCASE19,
     FSC.NAME: FSC
 }
-N_CLASSES = {'esc10': 5, 'esc50': 10, "fsc": 9}
+N_CLASSES = {'esc10': 5, 'esc50': 10, "dcase19": 3, "fsc": 9}
 BACKBONES = {
     'esc10': ["resnet18"] * 2 + ["resnet34"] * 1 + ["vgg16.tv_in1k"] * 1 + ["mixer_b16_224.miil_in21k_ft_in1k"] * 1,
     'esc50': ["resnet18"] * 4 + ["resnet34"] * 2 + ["vgg16.tv_in1k"] * 2 + ["mixer_b16_224.miil_in21k_ft_in1k"] * 2,
+    "dcase19": ["resnet18", "vgg16.tv_in1k", "mixer_b16_224.miil_in21k_ft_in1k"],
     "fsc": ["resnet18"] * 3 + ["resnet34"] * 2 + ["vgg16.tv_in1k"] * 2 + ["mixer_b16_224.miil_in21k_ft_in1k"] * 2,
 }
 
---
+++ datasets/my_dcase19.py
@@ -0,0 +1,144 @@
+# My DCASE19 dataset
+from utils import create_if_not_exists
+import numpy as np
+import pandas as pd
+import h5py
+import torchvision.transforms as transforms
+from PIL import Image
+from pathlib import Path
+from .utils.continual_dataset import ContinualDataset, get_previous_train_loader, store_masked_loaders
+from .utils.validation import get_train_val
+from augmentations import get_aug
+import librosa
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
+class MYDCASE19():
+    def __init__(self, data, targets, transform):
+        self.data = np.stack([np.array(image) for image in data])
+        self.targets = np.array(targets)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """
+        Gets the requested element from the dataset.
+        :param index: index of the element to be returned
+        :returns: tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img, mode='RGB')
+        original_img = img.copy()
+
+        img, img1, not_aug_img = self.transform(original_img)
+
+        if hasattr(self, 'logits'):
+            return (img, img1, not_aug_img), target, self.logits[index]
+
+        return (img, img1, not_aug_img),  target
+
+    def __len__(self):
+        return len(self.targets)
+
+
+def my_dcase19(name, transform=None):
+    """
+    Preprocesses the ESC10 dataset.
+    """
+    directory = f'data/{name}/'
+    create_if_not_exists(directory)
+
+    if os.path.exists(directory + "image_data.h5") and os.path.exists(directory + "target_data.h5"):
+        # Return from file itself
+        h5image = h5py.File(directory + "image_data.h5", "r")
+        image_data = h5image[name][:]
+        h5image.close()
+        h5target = h5py.File(directory + "target_data.h5", "r")
+        target_data = h5target[name][:]
+        h5target.close()
+
+        return MYDCASE19(image_data, target_data, transform)
+    else:
+        image_path = f"E:\\Projects\\ContinuousLearning\\HCL_Audio\\assets\{name}\\DCASE19.hdf5"
+        target_path = f"E:\\Projects\\ContinuousLearning\\HCL_Audio\\assets\{name}\\DCASE_Labels.csv"
+
+        data, targets = [], []
+
+        with h5py.File(image_path, "r") as f:
+            target_map = {}
+            df = pd.read_csv(target_path, header=None)
+            for _, row in df.iterrows():
+                target_map[row.iloc[0]] = row.iloc[1]
+
+            for filename in list(f.keys()):
+                S_img = Image.fromarray(f[filename][:].astype(np.uint8)).convert(
+                    'RGB').resize((255, 255))
+                data.append(S_img)
+                targets.append(target_map[filename])
+
+        # Cache to file
+        h5image = h5py.File(directory + "image_data.h5", "w")
+        image_data = h5image.create_dataset(name, data=data)
+        h5image.close()
+        h5target = h5py.File(directory + "target_data.h5", "w")
+        target_data = h5target.create_dataset(name, data=targets)
+        h5target.close()
+
+        return MYDCASE19(data, targets, transform)
+
+
+class DCASE19(ContinualDataset):
+    """
+    Returns a structured ESC50 class
+    """
+
+    NAME = "dcase19"
+    SETTING = "class-il"
+    N_CLASSES_PER_TASK = 3
+    N_TASKS = 3
+
+    def __init__(self):
+        super(DCASE19, self).__init__()
+
+    def get_data_loaders(self):
+        esc_norm = [[0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2615]]
+        transform = get_aug(train=True, mean_std=esc_norm,
+                            name="simsiam", image_size=32, cl_default=True)
+        test_transform = get_aug(train=False, train_classifier=False,
+                                 mean_std=esc_norm, name="simsiam", image_size=32, cl_default=True)
+
+        train_dataset = my_dcase19(self.NAME, transform=transform)
+        memory_dataset = my_dcase19(self.NAME, transform=test_transform)
+
+        train_dataset, test_dataset = get_train_val(
+            train_dataset, test_transform, self.NAME)
+        memory_dataset, _ = get_train_val(
+            memory_dataset, test_transform, self.NAME)
+
+        train, memory, test = store_masked_loaders(
+            train_dataset, test_dataset, memory_dataset, self)
+        return train, memory, test
+
+    def get_transform(self):
+        esc_norm = [[0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2615]]
+        transform = transforms.Compose(
+            [transforms.ToPILImage(),
+             transforms.RandomCrop(32, padding=4),
+             transforms.RandomHorizontalFlip(),
+             transforms.ToTensor(),
+             transforms.Normalize(*esc_norm)
+             ]
+        )
+
+        return transform
+
+    def not_aug_dataloader(self, batch_size):
+        esc_norm = [[0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2615]]
+        transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(*esc_norm)])
+
+        train_dataset = my_dcase19(self.NAME, train=True, transform=transform)
+        train_loader = get_previous_train_loader(
+            train_dataset, batch_size, self)
+
+        return train_loader
